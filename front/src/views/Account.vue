79166457aa69b0390e4a681e5390301f73c1850e
uutaein <uutaein@gmail.com>
2020-06-11 21:56:37 +0900

    [style]: account 스타일 적용

--- front/src/views/Account.vue
+++ front/src/views/Account.vue
@@ -1,7 +1,55 @@
 /<template>
-    <v-app>
-        hi
-    </v-app>
+<v-app id="landing-page">
+    <v-content>
+      <v-container
+        background-color="primary"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="8"
+          >
+            <v-card class="elevation-6">
+              <v-toolbar
+                color="#625265"
+                dark
+                flat
+              >
+                <v-toolbar-title>나의 계정</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -11,5 +59,13 @@ export default {
 </script>
 
 <style>
-
+   #landing-page {
+     /*background-image: url('../assets/hands_joined_team.jpeg');*/
+    background-color: #8f2c2c;
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+  }
 </style>
