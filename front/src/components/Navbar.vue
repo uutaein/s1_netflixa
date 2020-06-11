bb63be0ad9cd97ff5b10aa05dd0d6588ca419986
uutaein <uutaein@gmail.com>
2020-06-11 18:44:10 +0900

    [refactor]: navbar 컴포넌트화

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav>
+    <v-app-bar color="indigo accent-2" app flat>
+      <v-app-bar-nav-icon id="drawerIcon" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase white--text">
+        <span class="font-weight-light">영화</span>
+        <span>츄천츄천</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" color="indigo accent-2">
+      <v-layout column align-center>
+        <v-flex class="my-5">
+        </v-flex>
+      </v-layout>
+
+      <v-list flat>
+        <v-list-item v-for="link in links" :key="link.route" link :to="link.route" exact="true">
+          <v-list-item-icon>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{link.text}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      drawer: false,
+      links: [
+        { icon: 'mdi-home', text: 'Home', route: '/' },
+        {
+          icon: 'mdi-filmstrip',
+          text: '영화 추천받기'
+          // route: '/movies-playing-now'
+        },
+        {
+          icon: 'mdi-trophy',
+          text: 'Top Rated Movies'
+          // route: '/top-rated-movies'
+        },
+
+        {
+          icon: 'mdi-medal',
+          text: 'Top Rated Shows'
+          // route: '/top-rated-shows'
+        },
+        {
+          icon: 'mdi-information',
+          text: '내 계정'
+          // route: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
