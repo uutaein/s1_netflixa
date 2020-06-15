30e7920da5bf0603dbb2e1edce5c1e97d97cb6f2
uutaein <uutaein@gmail.com>
2020-06-15 17:00:18 +0900

    [feat(F)]: comment 작성중

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user_id: '',
     user_name: '',
     base_url: '',
     isLoggedin: false
@@ -15,6 +16,9 @@ export default new Vuex.Store({
     },
     usernameSave (state, username) {
       state.user_name = username
+    },
+    useridSave (state, userID) {
+      state.user_id = userID
     },
     Login (state) {
       state.isLoggedin = true
