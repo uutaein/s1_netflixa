fea2765fbfa850dc7644eacddefb1ee550025e5d
uutaein <uutaein@gmail.com>
2020-06-15 14:55:33 +0900

    [chore(F)]: signup + vuex 에 데이터 저장

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -5,15 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user_id: '',
-    base_url: ''
+    user_name: '',
+    base_url: '',
+    isLoggedin: false
   },
   mutations: {
     urlSave (state, url) {
       state.base_url = url
     },
-    idSave (state, id) {
-      state.user_id = id
+    usernameSave (state, username) {
+      state.user_name = username
+    },
+    Login (state) {
+      state.isLoggedin = true
+    },
+    Logout (state) {
+      state.isLoggedin = false
     }
   },
   actions: {
