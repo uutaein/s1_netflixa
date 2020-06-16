098b9f4a114726cd7a6cda193023b36441bbea9a
Kanggyu <macaron61@gmail.com>
2020-06-16 15:33:35 +0900

    [fix(B)]:serializers 수정

--- back/movies/serializers.py
+++ back/movies/serializers.py
@@ -5,7 +5,7 @@ from .models import Movie
 class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'title')
+        fields = ('id', 'title', 'poster_path', 'popularity', 'vote_average', 'release_date')
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +16,4 @@ class MovieSerializer(serializers.ModelSerializer):
 class MovieCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        exclude = ['like_users']
+        exclude = ['like_users', 'popularity', 'vote_count', 'vote_average']
