0748a2554190e042285b34bfe8c17a07c89da1f9
Kanggyu <macaron61@gmail.com>
2020-06-17 20:11:08 +0900

    [fix(F)]: 카드 크기 수정

--- front/src/components/MediaGrid.vue
+++ front/src/components/MediaGrid.vue
@@ -1,9 +1,9 @@
 <template>
   <v-layout row wrap>
     <v-flex xs12 sm6 md4 lg3 v-for="movie in movies" :key="movie.id">
-      <v-card hover class="ma-3"  height="35vh">
+      <v-card hover class="ma-3"  height="50vh">
         <v-layout column align-center fill-height class="text-center">
-          <v-img height="25vh" class="white--text align-end" :src="movie.poster_path">
+          <v-img height="30vh" class="white--text align-end" :src="movie.poster_path">
           <v-card-title class="font-weight-light" color="white">{{movie.title || movie.name}}</v-card-title>
           </v-img>
           <v-rating
